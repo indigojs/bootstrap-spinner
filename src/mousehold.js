9b3b140b8d086c512607b70417d0343a3f6d1161
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-03-31 01:03:24 +0200

    Trigger mousehold event

--- src/mousehold.js
+++ src/mousehold.js
@@ -1,20 +1,11 @@
-/**
- * jQuery mousehold plugin - fires an event while the mouse is clicked down.
- * Additionally, the function, when executed, is passed a single
- * argument representing the count of times the event has been fired during
- * this session of the mouse hold.
- *
- * @author Remy Sharp (leftlogic.com)
- * @date 2006-12-15
- * @example $("img").mousehold(200, function(i){  })
- * @desc Repeats firing the passed function while the mouse is clicked down
- *
- * @name mousehold
- * @type jQuery
- * @param Number timeout The frequency to repeat the event in milliseconds
- * @param Function fn A function to execute
- * @cat Plugin
- */
+/* ========================================================================
+ * mousehold.js v1.0.0
+ * http://github.com/indigojs
+ * ========================================================================
+ * Copyright 2014 Márk Sági-Kazár
+ * Original author 2006 Remy Sharp (leftlogic.com)
+ * Licensed under MIT (https://github.com/indigojs/bootstrap-spinner/blob/master/LICENSE)
+ * ======================================================================== */
 
 +function ($) {
   'use strict';
@@ -22,40 +13,46 @@
   // MOUSEHOLD PLUGIN DEFINITION
   // ===========================
 
-  $.fn.mousehold = function(timeout, fn) {
-    if (timeout && typeof timeout == 'function') {
-      fn = timeout
-      timeout = 100
-    }
+  $.fn.mousehold = function(timeout) {
+    timeout = typeof timeout !== 'undefined' ? timeout : 100;
+    var interval = 0
+    var fireStep = 0
+    var clearMousehold = undefined;
+    var e = $.Event('mousehold', { counter: 0 })
 
-    if (fn && typeof fn == 'function') {
-      var interval = 0
-      var fireStep = 0
-      var clearMousehold = undefined;
+    return this.each(function() {
+      $(this).mousedown(function() {
+        fireStep = 1
+        var t = this
 
-      return this.each(function() {
-        $(this).mousedown(function() {
-          fireStep = 1
-          var ctr = 0
-          var t = this
+        interval = setInterval(function() {
+          e.counter++
+          $(t).trigger(e)
+          fireStep = 2
+        }, timeout)
+      })
 
-          interval = setInterval(function() {
-            ctr++
-            fn.call(t, ctr)
-            fireStep = 2
-          }, timeout)
-        })
-
-        clearMousehold = function() {
-          clearInterval(interval)
-          if (fireStep == 1) fn.call(this, 1)
-          fireStep = 0
+      clearMousehold = function() {
+        clearInterval(interval)
+        if (fireStep == 1) {
+          e.counter = 1
+          $(this).trigger(e)
         }
 
-        $(this).mouseout(clearMousehold)
-        $(this).mouseup(clearMousehold)
-      })
-    }
+        fireStep = 0
+      }
+
+      $(this).mouseout(clearMousehold)
+      $(this).mouseup(clearMousehold)
+    })
   }
+
+  $(window).on('load', function () {
+    $('[data-on="mousehold"]').each(function() {
+      var $this = $(this)
+
+      $this.mousehold($this.data('timeout'))
+    })
+  })
 
 }(jQuery);
